805bf4a2b75652d8ad0ec4bc5801269040773e62
larisa <larisatazoa@yahoo.ca>
2020-09-15 15:55:03 -0500

    modify.sh

--- modify.sh
+++ modify.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-find /var/log/*.log -mtime +14 -exec chmod u+x {} \;
+#Description: Delete log files 14 days old
+#Author: Larisa
+#Date: 09/15/2020
+
+echo -e "\nDeleting files 14 days older\n"
+
+find /var/log/*.log -mtime +14 -exec rm -rf {} \;
 
